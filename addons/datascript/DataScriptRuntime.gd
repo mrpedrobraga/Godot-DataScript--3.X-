cb6ac88e57b7303f3a8ff3e30f5b4590d175a485
mrpedrobraga <mrhenribraga@gmail.com>
2023-10-15 20:25:59 -0300

    Made a new function to execute instructions.

--- addons/datascript/DataScriptRuntime.gd
+++ addons/datascript/DataScriptRuntime.gd
@@ -37,81 +37,97 @@ class ExecutionContext:
 	var last_result = null
 	var last_keyword = ""
 
-## Executes a routine.
-func execute(routine, _context: ExecutionContext = null):
-	if not routine is Array:
-		return routine
+## Executes a block of instructions.
+func execute(block, _context: ExecutionContext = null):
+	yield(get_tree(), "idle_frame")
+
+	# If trying to "execute" a string, number or boolean,
+	# just return it unchanged.
+	if not block is Array:
+		return block
 	
+	# Initialize context if missing
 	if not _context:
 		_context = ExecutionContext.new()
 
+	# You can detect that this routine is a 'block'
+	# by checking if the first element is an array
+	# or is the string ()
+	if not (block[0] is Array or (block[0] is String and block[0] == "()")):
+		return _execute_instruction(block, _context)
+
+	# From here on, `routine` is treated as an expression block,
+	# that is, an Array of instructions.
+
 	var instruction_index = 0
-	var instruction_count = routine.size()
+	var instruction_count = block.size()
 	var result
-	yield(get_tree(), "idle_frame")
 
 	while true:
 		# Break if overflow the instruction buffer
 		if instruction_index >= instruction_count:
 			break
 		
-		var instruction = routine[instruction_index]
+		var instruction = block[instruction_index]
+		result = _execute_instruction(instruction, _context)
 
-		if not instruction is Array:
-			# Allow booleans and numbers to be instructions
-			# which just return their value
-			result = instruction
-		else:
-			var i_keyword = instruction[0]
-			var i_params = instruction.slice(1, instruction.size())
-			
-			# Evaluate the parameters in case they're
-			# function calls!
-			var i_evaluated_params = []
-
-			# Meta functions won't pre-evaluate their parameters,
-			# so you'll need to call "execute" from inside them.
-			if not i_keyword in meta_functions:
-				for param in i_params:
-					var param_eval_result
-					if param is Array:
-						if param[0] is Array:
-							param_eval_result = execute(param.slice(1, param.size()), _context)
-						elif param[0] is String:
-							# Otherwise, it'll be a single instruction.
-							param_eval_result = execute([param], _context)
-						else:
-							param_eval_result = param
-						if param_eval_result is GDScriptFunctionState:
-							param_eval_result = yield(param_eval_result, "completed")
-					else:
-						# If a parameter is a literal, leave it be.
-						param_eval_result = param
-					i_evaluated_params.push_back(param_eval_result)
-			else:
-				i_evaluated_params = i_params
-
-			if not has_method(i_keyword):
-				push_error("No method found with the name '{name}'.".replace("{name}", i_keyword))
-			else:
-				var call_params = []
-				if i_keyword in meta_functions:
-					call_params.append(_context)
-				call_params.append_array(i_evaluated_params)
-				result = callv(i_keyword, call_params)
-
-				# If the called method was asynchronous, wait for its completion.
-				if result is GDScriptFunctionState:
-					result = yield(result, "completed")
-			
-			if not i_keyword in meta_functions:
-				_context.last_condition_check_was_successful = false
+		if result is GDScriptFunctionState:
+			result = yield(result, "completed")
 		
-			_context.last_keyword = i_keyword
 		_context.last_result = result
 		instruction_index += 1
 	
 	# Return the result of the last evaluated expression.
 	return result
+
+## Executes a single instruction.
+func _execute_instruction(instruction, _context: ExecutionContext):
+	if (not instruction is Array) or (not instruction[0] is String):
+		# If the instruction doesn't follow the correct form
+		# ["<i_name>", ...params]
+		return instruction
+	else:
+		var i_keyword = instruction[0]
+		var i_params = instruction.slice(1, instruction.size())
 		
+		# Evaluate the parameters in case they're
+		# function calls!
+		var i_evaluated_params = []
+
+		# Evaluating the parameters of the function cals.
+		# This allows you to use the result of a function call
+		# as the parameter to another function call.
+		if not i_keyword in meta_functions:
+			for param in i_params:
+				var param_eval_result = execute(param)
+				if param_eval_result is GDScriptFunctionState:
+					param_eval_result = yield(param_eval_result, "completed")
+					i_evaluated_params.push_back(param_eval_result)
+		# Meta functions won't pre-evaluate their parameters,
+		# so you'll need to call "execute" from inside them.
+		else:
+			i_evaluated_params = i_params
+
+		if not has_method(i_keyword):
+			push_error("No method found with the name '{name}'.".replace("{name}", i_keyword))
+			return null
+
+		var call_params = []
+		if i_keyword in meta_functions:
+			call_params.append(_context)
+		call_params.append_array(i_evaluated_params)
+		
+		var result = callv(i_keyword, call_params)
+		
+		# If the called method was asynchronous, wait for its completion.
+		if result is GDScriptFunctionState:
+			result = yield(result, "completed")
+		
+		# Update context
+		_context.last_keyword = i_keyword
+		if not i_keyword in meta_functions:
+			_context.last_condition_check_was_successful = false
+
+		# Return the result	
+		return result 
 
